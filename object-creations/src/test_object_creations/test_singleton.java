478d1989ee744db3149895a64cfa279c18f5c8a8
omaralsayed <42531544+omaralsayed@users.noreply.github.com>
2020-03-12 01:15:53 -0400

    Refactor JUnit codebase

--- object-creations/src/test_object_creations/test_singleton.java
+++ object-creations/src/test_object_creations/test_singleton.java
@@ -8,26 +8,31 @@ import singleton.ReadExcelInfo;
 
 class test_singleton {
 
-	@Test // File Processing
+	// Check file processing
+	@Test
 	void A_allArrayListsShouldContainSameNumberOfElements() {
 		ReadExcelInfo readFile = ReadExcelInfo.getInstance();
 		readFile.save("zipCode_info.xlsx");
 
+		// Assert statements
         assertEquals(readFile.getId().size(), readFile.getName().size(), "Number of elements must be the same");
         assertEquals(readFile.getName().size(), readFile.getSt().size(), "Number of elements must be the same");
         assertEquals(readFile.getSt().size(), readFile.getZipMin().size(), "Number of elements must be the same");
         assertEquals(readFile.getZipMin().size(), readFile.getZipMax().size(), "Number of elements must be the same");
 	}
 	
-	@Test // Singleton
+	// Check singleton pattern design
+	@Test
 	void B_memoryAddressShouldBeSame() {
 		ReadExcelInfo getInstanceOne = ReadExcelInfo.getInstance();
 		ReadExcelInfo getInstanceTwo = ReadExcelInfo.getInstance();
 		
+		// Assert statement		
 		assertEquals(getInstanceOne, getInstanceTwo, "Memory address must be the same");
 	}
 	
-	@Test // Singleton
+	// Check singleton pattern getters
+	@Test
 	void C_secondGetterShouldReturnAnEmptyArrayList() {
 		ReadExcelInfo getInstanceOne = ReadExcelInfo.getInstance();
 		getInstanceOne.save("zipCode_info.xlsx");
@@ -35,10 +40,12 @@ class test_singleton {
 		ReadExcelInfo getInstanceTwo = ReadExcelInfo.getInstance();
 		getInstanceTwo.save("zipCode_info.xlsx");
 		
+		// Remove all elements from the retrieved array
 		getInstanceOne.getId().clear();
 		
 		ArrayList<String> empty = new ArrayList<String>();
 		
+		// Assert statement				
 		assertEquals(getInstanceTwo.getId(), empty, "Data structures must be empty");
 	}
 
