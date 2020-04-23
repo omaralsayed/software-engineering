453ac2bd582e408479a94777f097ac3fce33d6ec
omaralsayed <42531544+omaralsayed@users.noreply.github.com>
2020-04-23 16:46:29 -0400

    Add unit test case

--- mutation-analysis/src/test_mutation_analysis/ResultDisplay.java
+++ mutation-analysis/src/test_mutation_analysis/ResultDisplay.java
@@ -1,46 +1,73 @@
 package test_mutation_analysis;
 
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.*;
-
-import business_logic.*;
-import DAO.*;
-import display.*;
 
 import org.junit.jupiter.api.Test;
 
-class ResultDisplay {
+import java.io.IOException;
 
+import DAO.FileIO;
+import business_logic.DistinguishZipCodeFactory;
+import business_logic.District;
+import business_logic.Selection;
+import business_logic.Phaser;
+
+class ResultDisplay {
+	
 	@Test
-	// check that results and levels are correctly matched
-	void test() {
+	/**
+	 * Check that results and levels are correctly matched
+	 * @throws IOException
+	 * @throws CloneNotSupportedException
+	 */
+	void A_test() throws IOException, CloneNotSupportedException {
 		
-		// Path
-		FileIO.ZipCodeInfo_InputPath = "zipCode_info.xlxs";
+		FileIO.ZipCodeInfo_InputPath = "zipCode_info.xlsx";
 		FileIO.PersonalAddress_InputPath = "input_sample3.txt";
+				
+		DistinguishZipCodeFactory DZ = new DistinguishZipCodeFactory();
 		
-		// Phaser for valid and invalid group
-		DistinguishZipCodeFactory dz = new DistinguishZipCodeFactory();
-		Phaser valid = dz.GetPhaser("valid");
-		Phaser invalid = dz.GetPhaser("invalid");
+//		Phaser valid = DZ.GetPhaser("valid");
+//		Phaser invalid = DZ.GetPhaser("invalid");
 		
 		// Phaser for selection (Blue and red)
-		Selection selection = new Selection();
-		Selection blue = (Selection) selection.SelectionBluePhaser(); 
-		Selection red = (Selection) selection.SelectionRedPhaser();
+//		Selection selection = new Selection();
+//		Selection blue = (Selection) selection.SelectionBluePhaser(); 
+//		Selection red = (Selection) selection.SelectionRedPhaser();
 		
 		// Phaser for region and district info
-		District district = new District(); 
+//		District district = new District(); 
 		
 		// Blue and red group
 		//assertNotEquals();
 		
-		// Division 
+	}
+	
+	@Test
+	/** 
+	 * Check that the number of valid and invalid people 
+	 * matches the number of their zipcodes
+	 * @throws IOException
+	 * @throws CloneNotSupportedException
+	 */
+	
+	void B_test() throws IOException, CloneNotSupportedException {
 		
+		FileIO.ZipCodeInfo_InputPath = "zipCode_info.xlsx";
+		FileIO.PersonalAddress_InputPath = "input_sample3.txt";
+				
+		DistinguishZipCodeFactory DZ = new DistinguishZipCodeFactory();
 		
-		// Region
+		Phaser valid = DZ.GetPhaser("valid");
+		Phaser invalid = DZ.GetPhaser("invalid");
 		
-		// Valid and invalid zipcode 
+		int valSize = valid.GetSize();
+		int invSize = invalid.GetSize();
+		
+		// Arithmetic mutation friendly
+		int totSize = valSize + invSize;
+		
+		assertEquals(totSize, 30);
 		
 	}
 
