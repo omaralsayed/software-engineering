cbbfeae0931e8a2b7fc43b02a2acafa7a565b0aa
omaralsayed <42531544+omaralsayed@users.noreply.github.com>
2020-04-23 15:44:41 -0400

    Create a JUnit test case

---
+++ mutation-analysis/src/test_mutation_analysis/ResultDisplay.java
@@ -0,0 +1,14 @@
+package test_mutation_analysis;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ResultDisplay {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
