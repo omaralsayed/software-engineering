fa333ebf74affaffd4ff38bdbcff1f28f3d2ae10
chloebeth <chloe.e.muncy@gmail.com>
2020-04-23 16:35:21 -0400

    Added init JUnit testing class

--- mutation-analysis/src/test_mutation_analysis/ResultDisplay.java
+++ mutation-analysis/src/test_mutation_analysis/ResultDisplay.java
@@ -1,14 +1,47 @@
 package test_mutation_analysis;
 
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.*;
+
+import business_logic.*;
+import DAO.*;
+import display.*;
 
 import org.junit.jupiter.api.Test;
 
 class ResultDisplay {
 
 	@Test
+	// check that results and levels are correctly matched
 	void test() {
-		fail("Not yet implemented");
+		
+		// Path
+		FileIO.ZipCodeInfo_InputPath = "zipCode_info.xlxs";
+		FileIO.PersonalAddress_InputPath = "input_sample3.txt";
+		
+		// Phaser for valid and invalid group
+		DistinguishZipCodeFactory dz = new DistinguishZipCodeFactory();
+		Phaser valid = dz.GetPhaser("valid");
+		Phaser invalid = dz.GetPhaser("invalid");
+		
+		// Phaser for selection (Blue and red)
+		Selection selection = new Selection();
+		Selection blue = (Selection) selection.SelectionBluePhaser(); 
+		Selection red = (Selection) selection.SelectionRedPhaser();
+		
+		// Phaser for region and district info
+		District district = new District(); 
+		
+		// Blue and red group
+		//assertNotEquals();
+		
+		// Division 
+		
+		
+		// Region
+		
+		// Valid and invalid zipcode 
+		
 	}
 
 }
